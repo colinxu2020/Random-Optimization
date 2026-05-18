66321eccea0b275543041716bd9c92508957eb47
Colinxu2020 <colinxu2020@gmail.com>
2026-05-18 16:46:45 +0800

    fix: resolve compatibility issues with Roadweaver

--- src/main/java/me/colinxu/randomoptimization/mixin/StructureCheckMixin.java
+++ src/main/java/me/colinxu/randomoptimization/mixin/StructureCheckMixin.java
@@ -2,15 +2,28 @@ package me.colinxu.randomoptimization.mixin;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.mojang.datafixers.DataFixer;
 import me.colinxu.randomoptimization.ChunkGeneratorStructureStateAccessor;
 import me.colinxu.randomoptimization.Config;
+import net.minecraft.core.RegistryAccess;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelHeightAccessor;
+import net.minecraft.world.level.biome.BiomeSource;
+import net.minecraft.world.level.chunk.ChunkGenerator;
 import net.minecraft.world.level.chunk.ChunkGeneratorStructureState;
+import net.minecraft.world.level.chunk.storage.ChunkScanAccess;
+import net.minecraft.world.level.levelgen.RandomState;
 import net.minecraft.world.level.levelgen.structure.Structure;
 import net.minecraft.world.level.levelgen.structure.StructureCheck;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplateManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Optional;
 
@@ -31,17 +44,39 @@ public class StructureCheckMixin implements ChunkGeneratorStructureStateAccessor
                     target="Lnet/minecraft/world/level/levelgen/structure/Structure;findValidGenerationPoint(Lnet/minecraft/world/level/levelgen/structure/Structure$GenerationContext;)Ljava/util/Optional;")
     )
     private Optional<Structure.GenerationStub> getGenerationPointIfStructureChunk(Structure instance, Structure.GenerationContext pContext, Operation<Optional<Structure.GenerationStub>> original) {
-        if(!Config.structureLocateSpeedup){
+        if(!Config.structureLocateSpeedup || this.randomoptimization$generatorState == null){
             return original.call(instance, pContext);
         }
         var structureHolder=pContext.registryAccess().registryOrThrow(Registries.STRUCTURE).wrapAsHolder(instance);
-        if (this.randomoptimization$generatorState != null) {
-            for (var structurePlacement : this.randomoptimization$generatorState.getPlacementsForStructure(structureHolder)) {
-                if (structurePlacement.isStructureChunk(this.randomoptimization$generatorState, pContext.chunkPos().x, pContext.chunkPos().z)) {
-                    return original.call(instance, pContext);
-                }
+        for (var structurePlacement : this.randomoptimization$generatorState.getPlacementsForStructure(structureHolder)) {
+            if (structurePlacement.isStructureChunk(this.randomoptimization$generatorState, pContext.chunkPos().x, pContext.chunkPos().z)) {
+                return original.call(instance, pContext);
             }
         }
         return Optional.empty();
+    }
+
+    @Inject(method = "<init>", at=@At("RETURN"))
+    private void randomoptimization$captureGeneratorState(
+            ChunkScanAccess pStorageAccess,
+            RegistryAccess pRegistryAccess,
+            StructureTemplateManager pStructureTemplateManager,
+            ResourceKey<Level> pDimension,
+            ChunkGenerator pChunkGenerator,
+            RandomState pRandomState,
+            LevelHeightAccessor pHeightAccessor,
+            BiomeSource pBiomeSource,
+            long pSeed,
+            DataFixer pFixerUpper,
+            CallbackInfo ci
+    ) {
+        if (!Config.structureLocateSpeedup) {
+            return;
+        }
+
+        if (pHeightAccessor instanceof ServerLevel serverLevel) {
+            this.randomoptimization$generatorState =
+                    serverLevel.getChunkSource().getGeneratorState();
+        }
     }
 }
